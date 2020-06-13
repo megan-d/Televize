bff7a4ffb727ca0ad9a3d54cf1d03112eadf2156
Megan D <45672063+megan-d@users.noreply.github.com>
2020-06-12 22:07:27 -0600

    Begin to incorporate styled components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
 function Navbar() {
-  return <h1>Cinema Search</h1>;
+  const Header = styled.header`
+    background: red;
+    width: 100%;
+    margin: auto;
+    min-height: 10vh;
+    height: 10vh;
+  `;
+
+  return <Header>Cinema Search</Header>;
 }
 
 export default Navbar;
