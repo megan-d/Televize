f4449885f662c1fde88ede48fc11b3eb59853380
Megan D <45672063+megan-d@users.noreply.github.com>
2020-06-12 21:22:36 -0600

    Add state for movie genres and display popular movies on Landing page

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,13 +4,17 @@ import Searchbox from './search/Searchbox';
 import SearchResults from './search/SearchResults';
 import { key } from '../config';
 
-
 class Landing extends Component {
   constructor() {
     super();
     this.state = {
       searchfield: '',
       movies: [],
+      action: [],
+      comedy: [],
+      adventure: [],
+      drama: [],
+      horror: [],
       isLoading: true,
       isSearching: false,
     };
@@ -18,14 +22,37 @@ class Landing extends Component {
 
   componentDidMount() {
     try {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`)
-          .then((response) => response.json())
-          //Filter the movie results to inlclude those with specific genres and only include first 4 for each genre
-          .then((data) => this.setState({ movies: data.results.filter(el => el.genre_ids.includes(28)).slice(0, 4), isLoading: false, isSearching: false, searchfield: '' }));
-          
-      } catch (error) {
-        console.error(error);
-      }
+      fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+      )
+        .then((response) => response.json())
+        //Filter the movie results to inlclude those with specific genres and only include first 4 for each genre
+        .then((data) =>
+          this.setState({
+            movies: data.results,
+            action: data.results
+              .filter((el) => el.genre_ids.includes(28))
+              .slice(0, 4),
+            comedy: data.results
+              .filter((el) => el.genre_ids.includes(35))
+              .slice(0, 4),
+              adventure: data.results
+              .filter((el) => el.genre_ids.includes(12))
+              .slice(0, 4),
+              drama: data.results
+              .filter((el) => el.genre_ids.includes(18))
+              .slice(0, 4),
+              horror: data.results
+              .filter((el) => el.genre_ids.includes(27))
+              .slice(0, 4),
+            isLoading: false,
+            isSearching: false,
+            searchfield: '',
+          }),
+        );
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   onSearchChangeHandler = (e) => {
@@ -34,9 +61,13 @@ class Landing extends Component {
 
   fetchData = (query) => {
     try {
-      fetch(`https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`)
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`,
+      )
         .then((response) => response.json())
-        .then((data) => this.setState({ movies: data.results, isLoading: false }));
+        .then((data) =>
+          this.setState({ movies: data.results, isLoading: false }),
+        );
     } catch (error) {
       console.error(error);
     }
@@ -49,16 +80,24 @@ class Landing extends Component {
   };
 
   resetSearch = () => {
-      try {
-        fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`)
-          .then((response) => response.json())
-          //Filter the movie results to inlclude those with specific genres
-          .then((data) => this.setState({ movies: data.results.filter(el => el.genre_ids.includes(28)), isLoading: false, isSearching: false, searchfield: '' }));
-          
-      } catch (error) {
-        console.error(error);
-      }
-  }
+    try {
+      fetch(
+        `https://api.themoviedb.org/3/movie/top_ratedapi_key=${key}&language=en-US&page=1`,
+      )
+        .then((response) => response.json())
+        //Filter the movie results to inlclude those with specific genres
+        .then((data) =>
+          this.setState({
+            action: data.results.filter((el) => el.genre_ids.includes(28)),
+            isLoading: false,
+            isSearching: false,
+            searchfield: '',
+          }),
+        );
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   render() {
     return !this.state.isSearching ? (
@@ -67,12 +106,41 @@ class Landing extends Component {
           onSearchChangeHandler={this.onSearchChangeHandler}
           onSubmit={this.onSubmit}
         />
-        <p>Popular Action Movies</p>
-        <Movies movies={this.state.movies} isLoading={this.state.isLoading} />
+        <Movies
+          movies={this.state.action}
+          isLoading={this.state.isLoading}
+          genre={'Action'}
+        />
+        <Movies
+          movies={this.state.comedy}
+          isLoading={this.state.isLoading}
+          genre={'Comedy'}
+        />
+        <Movies
+          movies={this.state.adventure}
+          isLoading={this.state.isLoading}
+          genre={'Adventure'}
+        />
+        <Movies
+          movies={this.state.drama}
+          isLoading={this.state.isLoading}
+          genre={'Drama'}
+        />
+        <Movies
+          movies={this.state.horror}
+          isLoading={this.state.isLoading}
+          genre={'Horror'}
+        />
       </Fragment>
     ) : (
-        <SearchResults movies={this.state.movies} isLoading={this.state.isLoading} isSearching={this.state.isSearching} searchfield={this.state.searchfield} reset={this.resetSearch}/>
-    )
+      <SearchResults
+        movies={this.state.movies}
+        isLoading={this.state.isLoading}
+        isSearching={this.state.isSearching}
+        searchfield={this.state.searchfield}
+        reset={this.resetSearch}
+      />
+    );
   }
 }
 
--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,36 +1,38 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import Movie from './Movie';
 
 function Movies(props) {
-  
   return props.isLoading ? (
     <Spinner />
   ) : !props.isLoading && props.movies.status_code === 34 ? (
     <p>Your search did not return any results. Please try another search.</p>
   ) : (
     !props.isLoading &&
-    props.movies.length > 0 && 
-    <div className='cards'>
-        {props.movies.map((movie, index) => {
-          //Check to see if there's a poster image for the movie. If not, set the image to null so can display generic image.
-          let image;
-          movie.poster_path ? image = `https://image.tmdb.org/t/p/w200/${movie.poster_path}` : image = null;
+    props.movies.length > 0 && (
+      <Fragment>
+        <h2 className='genre-heading'>Popular {props.genre} Movies</h2>
+        <div className='cards'>
+          {props.movies.map((movie, index) => {
+            //Check to see if there's a poster image for the movie. If not, set the image to null so can display generic image.
+            let image;
+            movie.poster_path
+              ? (image = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`)
+              : (image = null);
             return (
-                
-                    
-                <Movie 
-                    key={index}
-                    id={movie.id}
-                    title={movie.title}
-                    summary={movie.overview}
-                    image={image}
-                />
-                
-            )
-        }) }
-    </div>
+              <Movie
+                key={index}
+                id={movie.id}
+                title={movie.title}
+                summary={movie.overview}
+                image={image}
+              />
+            );
+          })}
+        </div>
+      </Fragment>
+    )
   );
 }
 
