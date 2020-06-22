1faf1b7a7048e282c6e12e92f88766968e0d19e5
Megan D <45672063+megan-d@users.noreply.github.com>
2020-06-22 13:29:25 -0600

    Update button and poster styling

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -4,30 +4,51 @@ import Button from 'react-bootstrap/Button';
 function Movie(props) {
   return (
     <div className='card background'>
-      <img alt='show' src={props.image} className='poster'></img>
+      <img alt='show' src={props.image} className='poster mt-3'></img>
       <h1 className='movie-title'>{props.name}</h1>
       <div>
         {/* if isRec is true, Learn More button should run a getRecDetails function that keeps isRec true. Then, will want back button to go back to the recommedations list  */}
         {props.isRec ? (
-          <Button variant='warning' onClick={() => props.getRecDetails(props.id)}>
+          <Button
+            size='sm'
+            style={{ maxWidth: '120px' }}
+            variant='warning'
+            onClick={() => props.getRecDetails(props.id)}
+            className='px-1 mb-3 mx-1'
+          >
             Learn More
           </Button>
         ) : (
-          <Button variant='warning' onClick={() => props.getDetails(props.id)}>
+          <Button
+            size='sm'
+            style={{ maxWidth: '120px' }}
+            variant='warning'
+            onClick={() => props.getDetails(props.id)}
+            className='px-1 mb-3 mx-1'
+          >
             Learn More
           </Button>
         )}
-        
+
         {props.isSearching ? (
           <Button
+            size='sm'
+            style={{ maxWidth: '120px' }}
             variant='warning'
             onClick={() => props.findSimilarSearch(props.id)}
+            className='px-1 mb-3 mx-1'
           >
-            Find Similar Shows
+            Similar Shows
           </Button>
         ) : (
-          <Button variant='warning' onClick={() => props.findSimilar(props.id)}>
-            Find Similar Shows
+          <Button
+            size='sm'
+            style={{ maxWidth: '120px'}}
+            variant='warning'
+            onClick={() => props.findSimilar(props.id)}
+            className='px-1 mb-3 mx-1'
+          >
+            Similar Shows
           </Button>
         )}
       </div>
