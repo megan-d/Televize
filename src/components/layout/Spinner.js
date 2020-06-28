e845bd35638d488813a23deff678c7e5382aba4a
Megan D <45672063+megan-d@users.noreply.github.com>
2020-06-27 21:52:08 -0600

    Begin to incorporate styled components

--- src/components/layout/Spinner.js
+++ src/components/layout/Spinner.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledSpinner = styled.div`
+  border: 10px solid #fdc108;
+  border-top: 10px solid black;
+  border-radius: 50%;
+  width: 80px;
+  height: 80px;
+  animation: spin 1s linear infinite;
+  margin: 0 auto;
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }}
+`;
 
 const Spinner = () => {
   return (
     <div className='full-page'>
-      <div className='spinner'></div>
+      <StyledSpinner />
     </div>
       
   );
--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -2,36 +2,37 @@ import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Spinner from '../layout/Spinner';
 import Movie from './Movie';
+import styled from 'styled-components';
+
+const StyledCards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
 
 const Movies = (props) => {
   return props.isLoading ? (
     <Spinner />
   ) : props.isSearching && props.shows.length === 0 ? (
-    <p className='text-center text-results'>Your search did not return any results. Please try another search.</p>
+    <p className='text-center text-results'>
+      Your search did not return any results. Please try another search.
+    </p>
   ) : (
     !props.isLoading &&
     props.shows.length > 0 && (
       <Fragment>
-        
-        <div className='cards'>
+        <StyledCards>
           {props.shows.map((show, index) => {
-            //Check to see if there's a poster image for the movie. If not, set the image to null so can display generic image.
-            let image;
-            show.poster_path
-              ? (image = `https://image.tmdb.org/t/p/w185/${show.poster_path}`)
-              : (image = 'none');
-              let background;
-              show.backdrop_path
-                ? (background = `https://image.tmdb.org/t/p/w1280/${show.backdrop_path}`)
-                : (background = 'none');
             return (
               <Movie
                 key={index}
                 id={show.id}
                 name={show.name}
                 summary={show.overview}
-                image={image}
-                background={background}
+                image={`https://image.tmdb.org/t/p/w185/${show.poster_path}`}
+                background={`https://image.tmdb.org/t/p/w1280/${show.backdrop_path}`}
                 getDetails={props.getDetails}
                 getRecDetails={props.getRecDetails}
                 findSimilar={props.findSimilar}
@@ -40,11 +41,11 @@ const Movies = (props) => {
               />
             );
           })}
-        </div>
+        </StyledCards>
       </Fragment>
     )
   );
-}
+};
 
 Movies.propTypes = {
   isLoading: PropTypes.bool.isRequired,
