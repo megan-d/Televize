c6820b5bfd10101644b21e6c92d070c98d8e35cb
Megan D <45672063+megan-d@users.noreply.github.com>
2020-07-03 21:54:13 -0600

    Modify sendFile

--- api/server.js
+++ api/server.js
@@ -136,7 +136,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../build'));
   //Return all requests to react app
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    // res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    res.sendFile('index.html', { root: path.join(__dirname, '../build') });
   });
 }
 
