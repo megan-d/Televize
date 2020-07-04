1073116d3722b54dc4efc86bfea91cf1244184d8
Megan D <45672063+megan-d@users.noreply.github.com>
2020-07-03 21:48:14 -0600

    Update dirname

--- api/server.js
+++ api/server.js
@@ -136,7 +136,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../build'));
   //Return all requests to react app
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
   });
 }
 
